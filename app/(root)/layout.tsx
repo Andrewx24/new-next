81f4284f30a5476c1e5d81028ce157b4a625a14a
Andrew Aliaj <andyaliaj@gmail.com>
2024-10-11 15:52:56 -0400

    updates

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+      <main className="background-light850_dark100 relative">
+        
+        {children}
+      </main>
+    )
+  }
+  
+  export default Layout
